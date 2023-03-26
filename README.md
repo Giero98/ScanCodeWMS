e72b4022e90db21dd693934a13ee9504e178c03e
Giero98 <b.gieras98@gmail.com>
2023-03-26 16:32:27 +0200

    Sending files over BT
    - added link provider to the folder with application files
    - removed display to enable BT outside app
    - Added intentions to enable BT
    - added BT enable listener
    - Changed reference to files using FileProvider
    - Added intentions to send files via BT
    - Added an explanation to the Readme on how to receive files for windows

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 ## Overview
 *short description*
 
+If you want to send files via Bluetooth to a device with the Windows operating system,
+you need to perform step 3 from the given list of steps on the given device:\
+- In Bluetooth & other devices settings, select Send or receive files via Bluetooth > Receive files.\
+[Receiving files via windows][receiving_files_via_windows]
+
 ## Used technologies/libraries
 
 Scanning codes using the library: [Code Scanner][code_scanner] which is licensed MIT License
@@ -16,6 +21,7 @@ Scan Code WMS created as part of an internship for Inn-Tek Sp. z o.o.\
 ![alt text][inn_tek_logo]\
 [GNU GPLv3][gnu_gplv3_link] © Bartosz Gieras
 
+[receiving_files_via_windows]: https://support.microsoft.com/pl-pl/topic/odbieranie-plik%C3%B3w-przez-bluetooth-d8da2667-e79b-744c-c135-f58af38fc3ba
 [code_scanner]: https://github.com/yuriy-budiyev/code-scanner
 [gnu_gplv3_logo]: https://www.gnu.org/graphics/gplv3-127x51.png "GNU GPLv3"
 [gnu_gplv3_link]: https://www.gnu.org/licenses/gpl-3.0.html
--- app/src/main/java/com/inn_tek/scancodewms/Bt.java
+++ app/src/main/java/com/inn_tek/scancodewms/Bt.java
@@ -19,22 +19,76 @@
 
 package com.inn_tek.scancodewms;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.util.ArrayList;
 
-public class Bt {
+public class Bt extends AppCompatActivity {
     Context context;
 
     public Bt(Context context){
         this.context = context;
-        //first connect or not
-        startSendingFiles();
+        checkBtIsOn();
     }
+
+    void checkBtIsOn() {
+        if (!Constants.bluetoothAdapter.isEnabled())
+            initiateBtActivation();
+        else
+            startSendingFiles();
+    }
+
+    void initiateBtActivation() {
+        startListenForBtStateChange();
+        launchIntentToEnableBT();
+    }
+
+    void startListenForBtStateChange()
+    {
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        context.getApplicationContext().registerReceiver(bluetoothStateReceiver, filter);
+    }
+    @SuppressLint("MissingPermission")
+    void launchIntentToEnableBT()
+    {
+        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.getApplicationContext().startActivity(intent);
+    }
+
+    final BroadcastReceiver bluetoothStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+
+                switch(state) {
+                    case BluetoothAdapter.STATE_ON:
+                        Toast.makeText(context, "BT On", Toast.LENGTH_SHORT).show();
+                        context.unregisterReceiver(this);
+                        startSendingFiles();
+                        break;
+                    case BluetoothAdapter.STATE_OFF:
+                        Toast.makeText(context, "BT Off", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }
+    };
 
     void startSendingFiles()
     {
@@ -44,9 +98,9 @@ public class Bt {
         if(folderIsEmpty(files))
         {
             ((Activity) context).runOnUiThread(() ->
-                    Toast.makeText(context, "There are no files to send", Toast.LENGTH_SHORT).show());
+                    Toast.makeText(context, "There are no file to send", Toast.LENGTH_SHORT).show());
         } else {
-            sendFiles(files);
+            prepareAndSendFiles(files);
         }
     }
 
@@ -55,18 +109,32 @@ public class Bt {
         return files.length == 0;
     }
 
-    void sendFiles(File[] files)
+    void prepareAndSendFiles(File[] files)
+    {
+        ArrayList<Uri> filesList = prepareFilesListToSend (files);
+        sendFilesViaBt(filesList);
+    }
+
+    ArrayList<Uri>  prepareFilesListToSend (File[] files)
     {
         ArrayList<Uri> filesList = new ArrayList<>();
         for (File file : files) {
             if (file.isFile()) {
-                Uri uri = Uri.fromFile(file);
+                Uri uri = FileProvider.getUriForFile(context, context.getPackageName() + ".fileprovider", file);
                 filesList.add(uri);
             }
         }
+        return filesList;
+    }
 
-        ((Activity) context).runOnUiThread(() ->
-                Toast.makeText(context, "Sending will start here", Toast.LENGTH_SHORT).show());
-
+    void sendFilesViaBt(ArrayList<Uri> filesList)
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        intent.setType("*/*");
+        intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, filesList);
+        intent.setPackage("com.android.bluetooth");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.getApplicationContext().startActivity(intent);
     }
 }
